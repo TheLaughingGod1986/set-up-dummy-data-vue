6bffd99c58d1060ebb5d9bc810ad71afb4bb83b1
benOatsInforma <benjamingraham.c.oats@informa.com>
2018-11-30 16:31:42 +0000

    set up dummy data in VUE

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+
+import store from './store'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  store
+});
